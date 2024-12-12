383222532327e5c696eb9f55c53a98036980d907
Thomas Renken <torenken@gmail.com>
2024-12-12 17:23:12 +0100

    add auth for page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import {Authenticator} from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
 import App from './App.tsx'
+import outputs from '../amplify_outputs.json';
+import './index.css'
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </StrictMode>,
 )
